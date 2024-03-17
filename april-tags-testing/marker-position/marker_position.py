11f1f93029bcd802d77f1a9b337c2360abababb7
Dylan Marin <dylanjmmarin@gmail.com>
2024-03-17 15:46:00 -0400

    get markers in reference frame of marker id 9. draw axis according to those positions

--- april-tags-testing/marker-position/marker_position.py
+++ april-tags-testing/marker-position/marker_position.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 from cv2 import aruco
 import argparse
+from collections import defaultdict
 
 def load_dict():
     data = np.load('../markers-dict/dictionary.npz')
@@ -24,7 +25,7 @@ def create_detector_params():
     # adaptiveThreshWinSizeStep = 10
     return detector_params
 
-def draw_cube(img, R, t, camera_mat, dist_coef, color):
+def draw_cube(img, R, t, camera_mat, dist_coef, color, sidelength = 5.6):
     '''
     Referenced from: 
         How to draw 3D Coordinate Axes with OpenCV for face pose estimation?
@@ -32,7 +33,7 @@ def draw_cube(img, R, t, camera_mat, dist_coef, color):
     '''
     # unit is mm
     rotV, _ = cv.Rodrigues(R)
-    points = np.float32([[-2.8, 2.8, 0],  [-2.8,-2.8,0],[2.8, -2.8, 0], [2.8, 2.8, 0], [-2.8, 2.8, -5.5],  [-2.8,-2.8, -5.5],[2.8, -2.8, -5.5], [2.8, 2.8, -5.5]]).reshape(-1, 3)
+    points = np.float32([[-sidelength/2, sidelength/2, 0],  [-sidelength/2,-sidelength/2,0],[sidelength/2, -sidelength/2, 0], [sidelength/2, sidelength/2, 0], [-sidelength/2, sidelength/2, -sidelength],  [-sidelength/2,-sidelength/2, -sidelength],[sidelength/2, -sidelength/2, -sidelength], [sidelength/2, sidelength/2, -sidelength]]).reshape(-1, 3)
     axisPoints, _ = cv.projectPoints(points, rotV, t, camera_mat, dist_coef)
 
     # draw cube edges onto screen
@@ -54,14 +55,59 @@ def draw_cube(img, R, t, camera_mat, dist_coef, color):
     return img
 
 
-ID_TO_COLOR_STRING = {
-    0: "green",
-    1: "orange",
-    2: "yellow",
-    3: "red",
-    4: "blue",
-    5: "white"
-}
+def inversePerspective(rvec, tvec):
+    ''' 
+    Get the inverse perspective of the rvec and tvec
+    Referenced from:
+    https://aliyasineser.medium.com/calculation-relative-positions-of-aruco-markers-eee9cc4036e3
+    '''
+    R, _ = cv.Rodrigues(rvec)
+    R = np.matrix(R).T
+    invTvec = np.dot(R, np.matrix(-tvec))
+    invRvec, _ = cv.Rodrigues(R)
+    return invRvec, invTvec
+
+
+def relativePosition(rvec1, tvec1, rvec2, tvec2):
+    """ 
+    Get relative position for rvec2 & tvec2. Compose the returned rvec & tvec to use composeRT with rvec2 & tvec2 
+    Referenced from:
+    https://aliyasineser.medium.com/calculation-relative-positions-of-aruco-markers-eee9cc4036e3
+    """
+    rvec1, tvec1 = rvec1.reshape((3, 1)), tvec1.reshape((3, 1))
+    rvec2, tvec2 = rvec2.reshape((3, 1)), tvec2.reshape((3, 1))
+    # Inverse the second marker
+    invRvec, invTvec = inversePerspective(rvec2, tvec2)
+    info = cv.composeRT(rvec1, tvec1, invRvec, invTvec)
+    composedRvec, composedTvec = info[0], info[1]
+    composedRvec = composedRvec.reshape((3, 1))
+    composedTvec = composedTvec.reshape((3, 1))
+    return composedRvec, composedTvec
+
+
+def get_marker_positions_from_base_marker(marker_rvecs, marker_tvecs):
+    '''
+    Get the position of the markers from the base marker
+    '''
+    base_marker_id = 9
+    base_marker_rvec = marker_rvecs[base_marker_id]
+    base_marker_tvec = marker_tvecs[base_marker_id]
+
+    output_rvecs = defaultdict()
+    output_tvecs = defaultdict()
+
+    for marker_id, rvec in marker_rvecs.items():
+        if marker_id != base_marker_id:
+            tvec = marker_tvecs[marker_id]
+            composedRvec, composedTvec = relativePosition(rvec, tvec,base_marker_rvec, base_marker_tvec)
+            output_rvecs[marker_id] = composedRvec.ravel()
+            output_tvecs[marker_id] = composedTvec.ravel()
+            print(f"Marker {marker_id} position: ")
+            print(f"x: {composedTvec[0][0]}")
+            print(f"y: {composedTvec[1][0]}")
+            print(f"z: {composedTvec[2][0]}")
+
+    return output_rvecs, output_tvecs
 
 ID_TO_COLOR = {
     0: (0, 255, 0),
@@ -69,14 +115,20 @@ ID_TO_COLOR = {
     2: (0, 255, 255),
     3: (0, 0, 255),
     4: (255, 0, 0),
-    5: (255, 255, 255)
+    5: (255, 255, 255),
+    6: (255, 100, 255),
+    7: (255, 100, 255),
+    8: (255, 100, 255),
+    9: (255, 100, 255)
 }
+
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get the position of the marker")
     parser.add_argument("-c", "--camera", type=int, help="Enter camera number")
-    parser.add_argument("-m", "--marker_size", type=float, help="Enter marker size in centimeters")
+    parser.add_argument("-m", "--marker_size", type=float, help="Enter the large marker size in centimeters")
     args = parser.parse_args()
 
     if args.camera is None:
@@ -87,12 +139,21 @@ if __name__ == "__main__":
         print(f"Using camera number {CAMERA_NUMBER}")
 
     if args.marker_size is None:
-        MARKER_SIZE = 1.5 # centimeters
-        print(f"Using default marker size {MARKER_SIZE}")
+        LARGE_MARKER_SIZE = 3 # centimeters
+        print(f"Using default large marker size {LARGE_MARKER_SIZE}")
     else:
-        MARKER_SIZE = args.marker_size
-        print(f"Using marker size {MARKER_SIZE}")
+        LARGE_MARKER_SIZE = args.marker_size
+        print(f"Using marker size {LARGE_MARKER_SIZE}")
     
+    def get_marker_size(marker_id):
+        '''
+        Get the marker size in centimeters
+        '''
+        if marker_id < 6:
+            return 1.5
+        else:
+            return LARGE_MARKER_SIZE
+
     marker_dict = load_dict()
     cam_mat, dist_coef = load_calib_data(CAMERA_NUMBER)
 
@@ -114,52 +175,46 @@ if __name__ == "__main__":
         if marker_corners:
             # from the docs:
             # "The camera pose relative to the marker is a 3d transformation from the marker coordinate system to the camera coordinate system."
-            rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
-                marker_corners, MARKER_SIZE, cam_mat, dist_coef
-            )
-            total_markers = range(0, marker_IDs.size)
+            
+            marker_rvecs = defaultdict()
+            marker_tvecs = defaultdict()
 
-            aruco.drawDetectedMarkers(frame, marker_corners, marker_IDs)
-
-            for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
-                cv.polylines(
-                    frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+            for i in range(len(marker_IDs)):
+                marker_ID = marker_IDs[i][0]
+                marker_size = get_marker_size(marker_ID)
+                rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
+                    marker_corners[i], marker_size, cam_mat, dist_coef
                 )
+                marker_rvecs[marker_ID] = rVec[0][0]
+                marker_tvecs[marker_ID] = tVec[0][0]
 
-                distance = np.sqrt(
-                    tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
-                )
+            for marker_ID, rVec in marker_rvecs.items():
+                tVec = marker_tvecs[marker_ID]
+                marker_size = get_marker_size(marker_ID)
+                # if marker_ID < 6:
+                #     # frame = draw_cube(frame, rVec, tVec, cam_mat, dist_coef, ID_TO_COLOR[marker_ID], sidelength=5.5)
+                #     point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec, tVec, marker_size, 4)
+                # else:
+                #     # frame = draw_cube(frame, rVec, tVec, cam_mat, dist_coef, ID_TO_COLOR[marker_ID], sidelength=marker_size)
+                #     point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec, tVec, marker_size, 4)
+                if marker_ID == 9:
+                    point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec, tVec, marker_size, 4)
 
-                # Draw the cube estimated from each marker
-                if ids[0] < 6:
-                    frame = draw_cube(frame, rVec[i], tVec[i], cam_mat, dist_coef, ID_TO_COLOR[ids[0]])
+            if [9] in marker_IDs and len(marker_IDs) > 1:
+                composedRvecs, composedTvecs = get_marker_positions_from_base_marker(marker_rvecs, marker_tvecs)
+                baseRvec = marker_rvecs[9]
+                baseTvec = marker_tvecs[9]
 
-                else:
-                    point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 2.9, 4)
+                for marker_ID, composedRvec in composedRvecs.items():
+                    composedTvec = composedTvecs[marker_ID]
 
-                '''
-                # cv.putText(
-                #     frame,
-                #     f"id: {ids[0]} Dist: {round(distance, 2)}",
-                #     top_right,
-                #     cv.FONT_HERSHEY_PLAIN,
-                #     1.3,
-                #     (0, 0, 255),
-                #     2,
-                #     cv.LINE_AA,
-                # )
-                # cv.putText(
-                #     frame,
-                #     f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} ",
-                #     bottom_right,
-                #     cv.FONT_HERSHEY_PLAIN,
-                #     1.0,
-                #     (0, 0, 255),
-                #     2,
-                #     cv.LINE_AA,
-                # )
-                # print(ids, "  ", corners)
-                '''
+                    info = cv.composeRT(composedRvec, composedTvec, baseRvec.T, baseTvec.T)
+                    TcomposedRvec, TcomposedTvec = info[0], info[1]
+
+                    cv.drawFrameAxes(frame, cam_mat, dist_coef, TcomposedRvec, TcomposedTvec, 1.5, 4)
+
+                    
+
 
 
         cv.imshow("frame", frame)
