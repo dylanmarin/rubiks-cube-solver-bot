0bd634b21ea926e8b9e47f43fea473d1b3bcd398
dylanmarin <dylanjmmarin@gmail.com>
2024-04-03 12:46:40 -0400

    updated script to use functions

--- robot-scripts/script_test.py
+++ robot-scripts/script_test.py
@@ -10,6 +10,87 @@ from time import sleep
 # To get started, open a terminal and type 'roslaunch interbotix_xsarm_control xsarm_control.launch robot_model:=wx250'
 # Then change to this directory and type 'python bartender.py  # python3 bartender.py if using ROS Noetic'
 
+
+def move_gripper_to_cube_home_position(bot, left=False, right=False):
+    roll_angle = 0
+    home_x = 0.14
+    home_pitch_adjust = 0
+    # home pitch adjust is only because our px150 base motor is not functioning perfectly
+
+    if left:
+        roll_angle = -np.pi/2
+        home_pitch_adjust = -0.05
+        home_x = 0.125
+    elif right:
+        roll_angle = np.pi/2
+        home_pitch_adjust = -0.05
+        home_x = 0.125
+
+    # above cube at home
+    bot.arm.set_ee_pose_components(
+        x=home_x,z=0.15, 
+        pitch=np.pi/2  + home_pitch_adjust, 
+        roll=0)        
+    
+    bot.arm.set_ee_pose_components(x=home_x,z=0.15, pitch=np.pi/2 + home_pitch_adjust, roll=roll_angle)
+
+    # grab cube at home (a little bit towards the far end)
+    bot.arm.set_ee_pose_components(x=home_x,z=0.045, pitch=np.pi/2+ home_pitch_adjust, roll=roll_angle)
+
+
+def raise_cube_from_home_position(bot, left=False, right=False):
+    roll_angle = 0
+    home_x = 0.14
+    home_pitch_adjust = 0
+    # home pitch adjust is only because our px150 base motor is not functioning perfectly
+
+    if left:
+        roll_angle = -np.pi/2
+        home_pitch_adjust = -0.05
+        home_x = 0.125
+    elif right:
+        roll_angle = np.pi/2
+        home_pitch_adjust = -0.05
+        home_x = 0.125
+
+
+    # raise cube directly up
+    bot.arm.set_ee_pose_components(x=home_x,z=0.15, pitch=np.pi/2+ home_pitch_adjust, roll=roll_angle)
+
+    # move cube forward and pitch it up
+    bot.arm.set_ee_pose_components(x=0.2,z=0.25, pitch=0, roll=0)
+
+def lower_cube_for_regripping(bot, bottom=False):
+    roll = 0
+    vertical_offset = 0
+    if bottom == True:
+        roll = np.pi
+        vertical_offset = 0.005
+
+    # rotate arm
+    bot.arm.set_ee_pose_components(x=0.2,z=0.25, pitch=0, roll=roll)
+
+    # lower cube and drop it
+    bot.arm.set_ee_pose_components(x=0.25, z=0.065 + vertical_offset, pitch=0, roll=roll)
+    bot.gripper.open()
+
+    # move gripper back
+    bot.arm.set_ee_pose_components(x=0.25,z=0.15, pitch=0, roll=roll)
+
+    # reset the roll
+    bot.arm.set_ee_pose_components(x=0.25,z=0.15, pitch=0, roll=0)
+
+    # point gripper down
+    bot.arm.set_ee_pose_components(x=0.175,z=0.07, pitch=np.pi/2, roll=0)
+
+def grip_cube_at_x(bot, x):
+    pitch_offset = -0.01
+    # move onto cube 
+    # add a pitch offset because of arm inconsistency
+    bot.arm.set_ee_pose_components(x=x,z=0.07, pitch=np.pi/2 + pitch_offset, roll=0)
+    bot.gripper.close()
+
+
 def main():
     bot = InterbotixManipulatorXS("px150", "arm", "gripper")
 
@@ -27,155 +108,26 @@ def main():
     bot.arm.go_to_sleep_pose()
 
     for i in range(2):
-            
+        # grabs cube at home
+        move_gripper_to_cube_home_position(bot, left=True)
 
-        roll_angle = 0
-        home_x = 0.14
-        home_pitch_adjust = 0
-
-        if i == 1:
-            left_side = False
-            right_side = True
-        if left_side:
-            roll_angle = -np.pi/2
-            home_pitch_adjust = -0.05
-            home_x = 0.125
-        elif right_side:
-            roll_angle = np.pi/2
-            home_pitch_adjust = -0.05
-            home_x = 0.125
-
-        # above cube at home
-        bot.arm.set_ee_pose_components(
-            x=home_x,z=0.1, 
-            pitch=np.pi/2  + home_pitch_adjust, 
-            roll=0)
-        
-    
-        bot.arm.set_ee_pose_components(x=home_x,z=0.1, pitch=np.pi/2 + home_pitch_adjust, roll=roll_angle)
-
-        # grab cube at home (a little bit towards the far end)
-        bot.arm.set_ee_pose_components(x=home_x,z=0.045, pitch=np.pi/2+ home_pitch_adjust, roll=roll_angle)
-        sleep(1)
         bot.gripper.close()
+        
+        # raises cube directly up and straightens it out
+        raise_cube_from_home_position(bot, left=True)
 
-        # raise cube directly up
-        bot.arm.set_ee_pose_components(x=home_x,z=0.15, pitch=np.pi/2+ home_pitch_adjust, roll=roll_angle)
+        # place the cube down for re-gripping
+        lower_cube_for_regripping(bot, bottom=True)
 
+        # grab the cube at the current x
+        current_cube_x = 0.28
+        grip_cube_at_x(bot, current_cube_x)
 
-        # move cube forward and pitch it up
-        bot.arm.set_ee_pose_components(x=0.2,z=0.25, pitch=0, roll=0)
-
-        # lower cube and drop it
-        bot.arm.set_ee_pose_components(x=0.25, z=0.065, pitch=0, roll=0)
+        move_gripper_to_cube_home_position(bot)
         bot.gripper.open()
 
-        # move gripper back
-        bot.arm.set_ee_pose_components(x=0.25,z=0.15, pitch=0, roll=0)
+        move_gripper_to_cube_home_position(bot)
 
-        # point gripper down
-        bot.arm.set_ee_pose_components(x=0.175,z=0.07, pitch=np.pi/2, roll=0)
-
-        # move onto cube (pitched - a little bit because of arm inconsistency)
-        bot.arm.set_ee_pose_components(x=0.28,z=0.07, pitch=np.pi/2 - 0.1, roll=0)
-        bot.gripper.close()
-
-        # return to home (first above)
-
-
-        bot.arm.set_ee_pose_components(
-            x=0.13,z=0.15, 
-            pitch=np.pi/2, 
-            roll=0)
-        
-
-        bot.arm.set_ee_pose_components(
-            x=0.13,z=0.045, 
-            pitch=np.pi/2, 
-            roll=0)
-        bot.arm.set_ee_pose_components(x=0.14,z=0.05, pitch=np.pi/2, roll=0)
-
-        # sleep(3)
-        bot.gripper.open()
-        bot.arm.go_to_sleep_pose()
-
-
-
-
-    # bot.arm.set_ee_pose_components(x=0.20,z=0.03, pitch=np.pi/2, roll=0)
-
-    # bot.arm.set_ee_cartesian_trajectory(roll=np.pi)
-    # bot.arm.set_ee_pose_components(x=0.27, z=0.07, pitch=0, roll=np.pi)
-
-    # bot.gripper.close()
-
-    # bot.arm.go_to_home_pose()
-    return
-
-    bot.gripper.open()
-
-
-
-
-    roll_angle = 0
-    front = False
-    back = False
-    if front or back: 
-        roll_angle = np.pi 
-
-    bot.arm.set_ee_pose_components(x=0.15, z=0.16, pitch=np.pi/2, roll=roll_angle)
-    bot.arm.set_ee_pose_components(x=0.15, z=0.03, pitch=np.pi/2, roll=roll_angle)
-    bot.gripper.set_pressure(0.8)
-    bot.gripper.close()
-
-    bot.arm.set_ee_pose_components(x=0.15, z=0.16, pitch=np.pi/2, roll=roll_angle)
-    bot.arm.set_ee_pose_components(x=0.27, z=0.06, pitch=0, roll=roll_angle)
-    bot.gripper.open()
-    # bot.arm.set_ee_pose_components(x=0.27, z=0.15, pitch=0, roll=roll_angle)
-    # bot.arm.set_ee_pose_components(x=0.27, z=0.05, pitch=np.pi/2, roll=roll_angle)
-    # bot.gripper.close()
-    # bot.arm.set_ee_pose_components(x=0.15, z=0.16, pitch=np.pi/2, roll=roll_angle)
-    # bot.arm.set_ee_pose_components(x=0.27, z=0.05, pitch=0, roll=roll_angle)
-    # bot.gripper.open()
-
-
-
-
-    # bot.arm.set_ee_pose_components(x=0.2, z=0.1, pitch=np.pi/2)
-
-    # bot.arm.set_ee_pose_components(x=0.25, z=0.3, pitch=0)
-
-    
-    # rotation = 1
-    # angle = np.pi/2 * rotation 
-    # bot.arm.set_ee_pose_components(x=0.25, z=0.3, pitch=0, roll=angle)
-    # bot.arm.set_ee_cartesian_trajectory(z=-0.2)
-
-
-    # bot.arm.set_ee_pose_components(x=0.2, z=0.01, pitch=0, roll=angle)
-
-    # bot.gripper.open()
-    # bot.arm.set_ee_pose_components(x=0.25, z=0.01, roll=angle)
-
-
-
-
-    # bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    # bot.arm.set_single_joint_position("waist", np.pi/2.0)
-    # bot.gripper.open()
-    # bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-    # bot.gripper.close()
-    # bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    # bot.arm.set_single_joint_position("waist", -np.pi/2.0)
-    # bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
-    # bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
-    # bot.arm.set_single_joint_position("waist", np.pi/2.0)
-    # bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-    # bot.gripper.open()
-    # bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    # bot.arm.go_to_home_pose()
-    # bot.arm.go_to_sleep_pose()
 
 if __name__=='__main__':
     main()
-
